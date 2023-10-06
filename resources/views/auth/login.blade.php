e17487d869cdc432c00423086dcd7c1162af554e
Ibnuamrithaher <93132829+Ibnuamrithaher@users.noreply.github.com>
2023-10-06 21:14:20 +0800

    UP

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -9,12 +9,14 @@
 <!-- Outer Row -->
 <div class="row justify-content-center">
     <div class="col-xl-10 col-lg-12 col-md-9">
-    
+
         <div class="card o-hidden border-0 shadow-lg my-5">
             <div class="card-body p-0">
                 <!-- Nested Row within Card Body -->
                 <div class="row">
-                    <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
+                    <div class="col-lg-6 d-none d-lg-block bg-login-image">
+                        <img src="{{ asset('logo_foging.jpeg') }}" width="100%" height="100%" alt="">
+                    </div>
                     <div class="col-lg-6">
                         <div class="p-5">
                             <div class="text-center">
@@ -43,12 +45,12 @@
                 </div>
             </div>
         </div>
-    
+
     </div>
 </div>
 
 @endsection
-    
+
 
 
 
